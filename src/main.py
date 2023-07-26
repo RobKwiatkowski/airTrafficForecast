a6652e87a2e52edfb6dcf49cc39320c2b463b25c
Robert <robert.kwiatkowski01@gmail.com>
2023-07-26 14:33:05 +0200

    refactor

--- src/main.py
+++ src/main.py
@@ -1,51 +1,49 @@
+"""The main script training the DNN model and doing predictions
+"""
+
 import keras
 import mlflow.keras
 import matplotlib.pyplot as plt
 
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from utils import plot_validation, train_val_split, prepare_data
 from models import build_model
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 
-PAX = prepare_data(data_path="air-traffic-passenger-statistics.csv",
-                   col1="Activity Period",
-                   col2="Passenger Count")
+PAX = prepare_data(
+    data_path="air-traffic-passenger-statistics.csv", col1="Activity Period", col2="Passenger Count"
+)
 
-n_steps_in = 12
-n_steps_out = 12
+N_STEP_IN = 12
+N_STEPS_OUT = 12
 
-X_train, y_train, X_valid, y_valid = train_val_split(PAX, n_steps_in, n_steps_out, 15)
+X_train, y_train, X_valid, y_valid = train_val_split(PAX, N_STEP_IN, N_STEPS_OUT, 15)
 
 print(X_train.shape, y_train.shape)
 print(X_valid.shape, y_valid.shape)
 
 with mlflow.start_run():
+    LOSS_METRIC = "mse"
+    LR = 0.002
+    EPOCHS = 2_000
+    PATIENCE = 1_000
+    N_FEATURES = 1
 
-    loss_metric = "mse"
-    lr = 0.002
-    epochs = 2_000
-    patience = 1_000
-    n_features = 1
+    callback_1 = EarlyStopping(monitor="val_loss", patience=PATIENCE)
+    callback_2 = ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=300, verbose=0)
 
-    callback_1 = EarlyStopping(monitor="val_loss",
-                               patience=patience)
-    callback_2 = ReduceLROnPlateau(monitor="val_loss",
-                                   factor=0.5,
-                                   patience=300,
-                                   verbose=0)
-
-    model = build_model(n_steps_in, n_steps_out, n_features)
+    model = build_model(N_STEP_IN, N_STEPS_OUT, N_FEATURES)
 
     # training
-    opt = keras.optimizers.Adam(learning_rate=lr)
-    model.compile(optimizer=opt, loss=loss_metric)
+    opt = keras.optimizers.Adam(learning_rate=LR)
+    model.compile(optimizer=opt, loss=LOSS_METRIC)
 
     mlflow.tensorflow.autolog()
     history = model.fit(
         X_train,
         y_train,
         validation_data=(X_valid, y_valid),
-        epochs=epochs,
+        epochs=EPOCHS,
         verbose=1,
         batch_size=80,
         callbacks=[callback_1, callback_2],
@@ -54,9 +52,9 @@ with mlflow.start_run():
 
     # model.save("my_model.keras")
 
-    mlflow.log_param("patience", patience)
-    mlflow.log_param("n_input", n_steps_in)
-    mlflow.log_param("loss_func", loss_metric)
+    mlflow.log_param("patience", PATIENCE)
+    mlflow.log_param("n_input", N_STEP_IN)
+    mlflow.log_param("loss_func", LOSS_METRIC)
 
     # mlflow.keras.log_model(model, "keras-model")
 
