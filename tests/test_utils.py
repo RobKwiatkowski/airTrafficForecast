b50b3b5839c216b84c6813c3d14ab9165501e322
Robert <robert.kwiatkowski01@gmail.com>
2023-07-26 10:12:50 +0200

    new tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,7 +13,20 @@ def test_split_sequence():
     assert outputs[-1][-1] == 5
 
 
-def test_prepare_data():
+def test_prepare_data_error():
     with pytest.raises(FileNotFoundError) as exc_info:
         exception_raised = prepare_data("not_exist_path", "A", "B")
         assert FileNotFoundError == exception_raised
+
+
+def test_prepare_data_read():
+    result = prepare_data("test_data/test_data_1.csv", "A", "B")
+    assert result.index.name == "A"
+    assert len(result.columns) == 1
+    assert result.columns[0] == "B"
+
+
+def test_prepare_data_scaling():
+    result = prepare_data("test_data/test_data_1.csv", "A", "B")
+    assert min(result["B"]) == 0
+    assert max(result["B"]) == 1
